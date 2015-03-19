51bdb4de4f900f2a6a7cf1dbeb4659e1e14afd37
Rinat Ibragimov <ibragimovrinat@mail.ru>
2015-03-20 00:47:50 +0300

    update ppapi headers to version 41

--- cpp-compat.h
+++ cpp-compat.h
@@ -2,3 +2,5 @@ typedef char bool;
 typedef struct PP_Var PP_Var;
 typedef struct PP_Point PP_Point;
 typedef struct PPP_Pdf_1 PPP_Pdf_1;
+typedef struct PPP_Pdf_1_1 PPP_Pdf_1_1;
+typedef struct PP_PdfPrintPresetOptions_Dev PP_PdfPrintPresetOptions_Dev;
